8905bbbab60d140f1de18df3131ceec1efdbd9ea
James Sandford Smith <jesandfordsmith@gmail.com>
2023-05-24 12:40:17 +0100

    made navbar resize on window resize

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 /* ----variables for navbar---- */
 const navContainer = document.getElementById("navbarcontainer");
 const mainPage = document.getElementById("mainpage");  //all scrolling is done on main not window so need to add event listenr to main
-let viewportHeight = window.innerHeight - window.innerHeight/2.5; //gets viewport height minus 10% of viewrport height
 const pastPage = document.getElementById("pastpage");
 const futurePage = document.getElementById("futurepage");
 const formPage = document.getElementById("contactpage");
 const navbarItems = document.getElementsByClassName("navbarliitem");
+let viewportHeight = window.innerHeight - window.innerHeight/2.5; //gets viewport height minus 10% of viewrport height
 /* ----event listeners for navbar---- */
-navContainer.style.width = (pastPage.getBoundingClientRect().width + "px");
+addEventListener("orientationchange",e => { //resizes navbar when orientation changed & corrects value of viewport height
+    resizeNavBar();
+    updateViewportHeight();
+}); 
+addEventListener("resize",e => {    //resizes navbar when window is resized & corrects value of viewport height
+    resizeNavBar();
+    updateViewportHeight();
+});  
+resizeNavBar();
+function resizeNavBar() {  //makes navbar same width as main to avoid overlapping scroll bar
+    navContainer.style.width = (pastPage.getBoundingClientRect().width + "px");
+}
+function updateViewportHeight() {
+    viewportHeight = window.innerHeight - window.innerHeight/2.5;
+}
 [...navbarItems].forEach((item,i)=> item.addEventListener("click", e => mainPage.scroll(0,window.innerHeight*(i+1))));  //add event listener to each anchor in navbar. when clicked sets main page scroll to that pages y coord. Need to do this way instead of scrollTo as we are scrolling main to make snap scroll work
 /* think event listener scroll is very resource intensive. should maybe change to set interval or some other method instead? */
 mainPage.addEventListener("scroll", e => {  //Hide navbar when on landing page otherwise shows and changes colour depending on page position
     if (mainPage.scrollTop>=viewportHeight) navContainer.classList.remove("hidden"); //shows navbar once you scroll to bottom of landing page
     else navContainer.classList.add("hidden"); //hides navbar when on landing page
-    if (formPage.getBoundingClientRect().top < viewportHeight); //checks if formpage is in view
-    else if (futurePage.getBoundingClientRect().top < viewportHeight) navContainer.style.backgroundColor = "var(--futurecolour)"; //checks if future page is in view
-    else if (pastPage.getBoundingClientRect().top < viewportHeight) navContainer.style.backgroundColor = "var(--pastcolour)"; //checks if past page is in view
+    if (formPage.getBoundingClientRect().top < viewportHeight) navContainer.classList.add("landingpagecolour"); //checks if formpage is in view
+    else if (futurePage.getBoundingClientRect().top < viewportHeight) { //checks if future page is in view
+        navContainer.classList.remove("landingpagecolour");
+        navContainer.style.backgroundColor = "var(--futurecolour)"; 
+    }
+    else if (pastPage.getBoundingClientRect().top < viewportHeight) { //checks if past page is in view
+        navContainer.classList.remove("landingpagecolour");
+        navContainer.style.backgroundColor = "var(--pastcolour)";
+    }
 })
 /* ----variables for landing page---- */
 const landingPast = document.getElementById("landingpast");
